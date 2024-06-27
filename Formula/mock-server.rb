a9949097ac7d59f99fd957b324f1ca24aaa1c227
Kim <deveye@126.com>
2024-06-27 18:34:47 +0800

    Update mock-server.rb

--- Formula/mock-server.rb
+++ Formula/mock-server.rb
@@ -1,7 +1,7 @@
 class MockServer < Formula
   desc "A mock server"
   homepage "https://github.com/hellodeveye/mock-server"
-  url "https://github.com/hellodeveye/mock-server/releases/download/v0.1.1/mock-server_Darwin_x86_64.tar.gz"
+  url "https://github.com/hellodeveye/mock-server/releases/download/v0.1.2/mock-server_Darwin_x86_64.tar.gz"
   sha256 "1f2359e12a1eece148eb0bfcdf3be460f4fe99108ca787fa55bf11857005ec08"
 
   def install
